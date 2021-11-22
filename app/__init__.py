8357023f15ae9e95dd0e78e7f0d8a31bac53ed71
geer26 <gergo.kurucz@gmail.com>
2021-11-22 13:50:45 +0100

    v0.1

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+from fernet import Secret
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,5 +13,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 login = LoginManager(app)
+
+secret = Secret(app)
 
 from app import routes, models
