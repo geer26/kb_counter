cad5f39d55dca101cdb08f61b01cec48c50bac2b
geer26 <gergo.kurucz@gmail.com>
2021-11-24 17:32:09 +0100

    v0.1

--- app/endpoints.py
+++ app/endpoints.py
@@ -61,7 +61,58 @@ class DeleteUser(Resource):
 
 
 
+#Documented!
+class Login(Resource):
+
+    def post(self):
+
+        # get data from posted json
+        json_data = request.get_json(force=True)
+
+        #Drop request if there is no payload
+        if not json_data:
+            return {'status': 4, 'message': 'LOGIN request must have params!'}, 400
+
+        #Drop request in case of missing param
+        if not json_data['username'] or not json_data['password']:
+            return {'status': 3, 'message': 'Username and password must be presented for login!'}, 400
+
+        username = json_data['username']
+        password = json_data['password']
+
+        if 'remember' in json_data.keys():
+            remember = json_data['remember']
+        else:
+            remember = False
+
+        user = Users.query.filter_by(username=str(username)).one_or_none()
+        if not user:
+            return {'status': 1, 'message': 'Invalid username!'}, 401
+        if not user.check_password(str(password)):
+            return {'status': 1, 'message': 'Invalid password!'}, 401
+        if not user.is_enabled:
+            return {'status': 1, 'message': 'User is disabled!'}, 401
+
+        login_user(user, remember=remember)
+        #user.authenticated = True
+        #user.expiration = int(datetime.now().timedelta(seconds=app.config['LOGIN_EXP_TIME']))
+        db.session.commit()
+
+        if user.is_superuser:
+            session['role'] = 'admin'
+        elif not user.is_superuser:
+            session['role'] = 'user'
+        session['_id'] = _create_identifier()
+
+        if user.is_superuser:
+            return {"status": 0, 'message': 'OK!', "redirect_to": "/admin/", 'role': 'admin', 'username': str(user.username)}, 200
+        else:
+            return {"status": 0, 'message': 'OK!', "redirect_to": "/user/", 'role': 'user', 'username': str(user.username)}, 200
+
+
+
 
 api.add_resource(AddUser, '/API/adduser')
 api.add_resource(GetAllData, '/API/query')
 api.add_resource(DeleteUser, '/API/deluser')
+api.add_resource(Login, '/API/login')
