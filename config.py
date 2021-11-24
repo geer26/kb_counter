a5f09f89b2351f75f5e8695c11d33064cfce1dc7
geer26 <gergo.kurucz@gmail.com>
2021-11-24 09:54:58 +0100

    v0.1

--- config.py
+++ config.py
@@ -16,10 +16,6 @@ class Config(object):
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
 
-    # ----------LOGS
-    LOG_FOLDER = os.path.join(basedir, 'log')
-
     # ----------DATABASE
-
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
