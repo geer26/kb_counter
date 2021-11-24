ed01dc1150de97ec2b2bf94276561be24eb6193f
geer26 <gergo.kurucz@gmail.com>
2021-11-24 18:42:51 +0100

    v0.1

--- app/endpoints.py
+++ app/endpoints.py
@@ -6,6 +6,7 @@ from flask_login import current_user, login_user, logout_user
 from flask import request, render_template, send_from_directory, session, Response
 from app import api, db
 from app.workers import pw_complexity, addsu, adduser, get_all_data, deluser
+from app.models import User
 
 
 
@@ -61,13 +62,17 @@ class DeleteUser(Resource):
 
 
 
-#Documented!
+#Done!
 class Login(Resource):
 
     def post(self):
 
         # get data from posted json
         json_data = request.get_json(force=True)
+
+        #Drop request if already logged in
+        if current_user.is_authenticated:
+            return {'status': 2, 'message': 'Already logged in!'}, 400
 
         #Drop request if there is no payload
         if not json_data:
@@ -77,37 +82,42 @@ class Login(Resource):
         if not json_data['username'] or not json_data['password']:
             return {'status': 3, 'message': 'Username and password must be presented for login!'}, 400
 
+        #Extract creditentials from payload
         username = json_data['username']
         password = json_data['password']
 
+        #Deprecated, will be False as default!
+        '''
         if 'remember' in json_data.keys():
             remember = json_data['remember']
         else:
             remember = False
+        '''
 
-        user = Users.query.filter_by(username=str(username)).one_or_none()
+        #Filter user by username
+        user = User.query.filter_by(username=str(username)).one_or_none()
+        #Return 401 by invalid username
         if not user:
             return {'status': 1, 'message': 'Invalid username!'}, 401
+        #Return 401 by invalid password
         if not user.check_password(str(password)):
             return {'status': 1, 'message': 'Invalid password!'}, 401
-        if not user.is_enabled:
-            return {'status': 1, 'message': 'User is disabled!'}, 401
 
-        login_user(user, remember=remember)
-        #user.authenticated = True
-        #user.expiration = int(datetime.now().timedelta(seconds=app.config['LOGIN_EXP_TIME']))
-        db.session.commit()
+        #If OK, log user in and return 200
+        login_user(user, remember=False)
+        return {"status": 0, 'message': f'User {user.username} logged in!'}, 200
 
-        if user.is_superuser:
-            session['role'] = 'admin'
-        elif not user.is_superuser:
-            session['role'] = 'user'
-        session['_id'] = _create_identifier()
 
-        if user.is_superuser:
-            return {"status": 0, 'message': 'OK!', "redirect_to": "/admin/", 'role': 'admin', 'username': str(user.username)}, 200
-        else:
-            return {"status": 0, 'message': 'OK!', "redirect_to": "/user/", 'role': 'user', 'username': str(user.username)}, 200
+
+#Documented!
+class Logout(Resource):
+    def get(self):
+
+        if not current_user or not current_user.is_authenticated:
+            return {'status': 1, 'message': 'Logout request refused!'}, 400
+        username = current_user.username
+        print(logout_user())
+        return {'status': 0, 'message': f'User {username} logged out!'}, 200
 
 
 
@@ -116,3 +126,4 @@ api.add_resource(AddUser, '/API/adduser')
 api.add_resource(GetAllData, '/API/query')
 api.add_resource(DeleteUser, '/API/deluser')
 api.add_resource(Login, '/API/login')
+api.add_resource(Logout, '/API/logout')
