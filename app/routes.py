542e02d707af9ac4918944051fcd356e463b54ac
geer26 <gergo.kurucz@gmail.com>
2021-11-24 14:25:21 +0100

    v0.1

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
+from flask import request, redirect, render_template, send_from_directory, send_file
+from flask_login import current_user, login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    if not current_user.is_authenticated:
+        return render_template('landingpage.html')
