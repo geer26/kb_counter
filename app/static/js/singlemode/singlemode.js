a92ec031640ee9b38cfcbc187f6efc6e34440a43
geer26 <gergo.kurucz@gmail.com>
2022-01-03 13:34:11 +0100

    v0.1

--- app/static/js/singlemode/singlemode.js
+++ app/static/js/singlemode/singlemode.js
@@ -1,7 +1,39 @@
 import {html, css, LitElement} from 'https://cdn.skypack.dev/pin/lit@v2.0.0-rc.2-TSkkpP2AxiJKOJvPcy1M/mode=imports,min/optimized/lit.js';
+//import {customElement, property, state} from 'https://cdn.skypack.dev/pin/lit@v2.0.0-rc.2-TSkkpP2AxiJKOJvPcy1M/mode=imports,min/optimized//decorators.js';
+
+//import {LitElement, html, css} from '../lit.min.js';
+//import {customElement, property, state} from '../lit/decorators.js';
+
+//import {LitElement, html} from 'lit';
 
 class MyElement extends LitElement {
+
   static properties = {
     version: {},
+    clicked: {},
   };
+
+  constructor() {
+    super();
+    this.version = 'STARTING';
+    this.clicked = 0;
+  }
+
+  handleClick(event){
+    this.clicked++;
+  }
+
+  render() {
+    return html`
+    <p>Welcome to the Lit tutorial!</p>
+    <p>This is the ${this.version} code.</p>
+    <p>Clicked ${this.clicked} times</p>
+    <span>
+        <button @click=${this.handleClick}>Increase</button>
+    </span>
+    `;
+  }
+
 }
+
+customElements.define('my-element', MyElement);
