0a91741bcdae4dbfd1907e7c4a5fe230557fbecc
geer26 <gergo.kurucz@gmail.com>
2022-01-04 05:51:52 +0100

    v0.1

--- app/static/js/singlemode/singlemode.js
+++ app/static/js/singlemode/singlemode.js
@@ -1,6 +1,4 @@
 
-//const axios = require('axios');
-
 import {html, css, LitElement} from 'https://cdn.skypack.dev/pin/lit@v2.0.0-rc.2-TSkkpP2AxiJKOJvPcy1M/mode=imports,min/optimized/lit.js';
 
 class MyElement extends LitElement {
