c84034fce5bc7dc0698ced8612c24f5f483e546e
geer26 <gergo.kurucz@gmail.com>
2022-01-05 15:18:30 +0100

    v0.1

--- app/static/js/singlemode/singlemode2.js
+++ app/static/js/singlemode/singlemode2.js
@@ -11,19 +11,42 @@ function fetch_event(id){
     data = JSON.stringify({eid: id});
     $.post('/API/fetch_s_event', data ,function(data){
         console.log('FETCHED DATA: ', data['data']);
-        $('#select-workout').hide();
+        app.data = data['data'];
+        $('#select-workout').remove();
         $('#app').show();
     }, 'json')
 }
 
+//------------------Vue app---------------------
+var app = new Vue({
 
+    delimiters: ['##', '##'],
+
+    el: '#app',
+
+    data: {
+        message: 'Hello Vue!',
+        data: {},
+    },
+
+    created: function(){},
+
+})
+
+
+//----------------Components--------------------
 Vue.component('button-counter', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+
+    name: 'counterbutton',
+
+    data: function () {
+        return {
+        count: 0
+        }
+    },
+
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+
 })
 
 new Vue({ el: '#components-demo' })
