8585036cbd7bc7cf17e96b53b9f7d4306e6777e7
geer26 <gergo.kurucz@gmail.com>
2022-01-05 11:56:30 +0100

    v0.1

--- app/static/js/singlemode/singlemode2.js
+++ app/static/js/singlemode/singlemode2.js
@@ -1,11 +1,30 @@
+
+
 $(document).ready(function(){
     $('.fixed-action-btn').floatingActionButton();
     $('select').formSelect();
     $('.tooltipped').tooltip();
 });
 
+
 function fetch_event(id){
-    console.log('EID TO FETCH: ', id);
     data = JSON.stringify({eid: id});
-    $.post('/API/fetch_s_event', data ,function(data){console.log(data['data']);}, 'json')
+    $.post('/API/fetch_s_event', data ,function(data){
+        console.log('FETCHED DATA: ', data['data']);
+        $('#select-workout').hide();
+        $('#app').show();
+    }, 'json')
 }
+
+
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
+new Vue({ el: '#components-demo' })
+
